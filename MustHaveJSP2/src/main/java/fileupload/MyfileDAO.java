e336b73604a492e48b5f24860171306832154dfa
YOUMIIII <youmi8255@naver.com>
2023-02-14 17:07:16 +0900

    파일업로드/다운로드 JAVA

---
+++ MustHaveJSP2/src/main/java/fileupload/MyfileDAO.java
@@ -0,0 +1,62 @@
+package fileupload;
+
+import java.util.List;
+import java.util.Vector;
+
+import common.DBConnPool;
+
+public class MyfileDAO extends DBConnPool{
+	public int insertFile(MyfileDTO dto) {		
+		int applyResult=0;
+		
+		try {
+			String query = "INSERT INTO myfile ( "
+					+ " idx, name, title, cate, ofile, sfile) "
+					+ " VALUES ( "
+					+ " seq_board_num.nextval, ?, ?, ?, ?, ?)";
+			
+			psmt = con.prepareStatement(query);
+			psmt.setString(1, dto.getName());
+			psmt.setString(2, dto.getTitle());
+			psmt.setString(3, dto.getCate());
+			psmt.setString(4, dto.getOfile());
+			psmt.setString(5, dto.getSfile());
+			
+			applyResult = psmt.executeUpdate();
+		}
+		catch (Exception e) {
+			System.out.println("INSERT 중 예외 발생");
+			e.printStackTrace();
+		}
+		return applyResult;
+	}
+	
+	public List<MyfileDTO> myFileList(){
+		List<MyfileDTO> fileList = new Vector<MyfileDTO>();
+		
+		//쿼리문 작성
+		String query = "SELECT * FROM myfile ORDER BY idx DESC";
+		try {
+			psmt = con.prepareStatement(query);
+			rs= psmt.executeQuery();
+			
+			while(rs.next()) {
+				MyfileDTO dto = new MyfileDTO();
+				dto.setIdx(rs.getString(1));
+				dto.setName(rs.getString(2));
+				dto.setTitle(rs.getString(3));
+				dto.setCate(rs.getString(4));
+				dto.setOfile(rs.getString(5));
+				dto.setSfile(rs.getString(6));
+				dto.setPostdate(rs.getString(7));
+				
+				fileList.add(dto);
+			}
+		} catch (Exception e) {
+			System.out.println("SELECT 시 예외 발생");
+			e.printStackTrace();
+		}
+		
+		return fileList;
+	}
+}
