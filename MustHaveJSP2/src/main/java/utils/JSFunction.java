688468680a2ef5325c2272ec5f59a7b11ef493a4
YOUMIIII <youmi8255@naver.com>
2023-02-15 17:45:12 +0900

    게시판 기능

--- MustHaveJSP2/src/main/java/utils/JSFunction.java
+++ MustHaveJSP2/src/main/java/utils/JSFunction.java
@@ -1,8 +1,41 @@
 package utils;
 
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.jsp.JspWriter;
 
 public class JSFunction {
+	public static void alertLocation(HttpServletResponse response, String msg, String url) {
+		try {
+			response.setContentType("text/html;charset=UTF-8");
+			PrintWriter writer = response.getWriter();
+			String script = ""
+						+"<script>"
+						+"	alert('" + msg + "');"
+						+"	location.href='" + url + "';"
+						+"</script>";
+			writer.println(script);
+		}
+		catch (Exception e) {
+		}
+	}
+	
+	public static void alertBack(HttpServletResponse response, String msg) {
+		try {
+			response.setContentType("text/html;charset=UTF-8");
+			PrintWriter writer = response.getWriter();
+			String script = ""
+						+"<script>"
+						+"	alert('" + msg + "');"
+						+"	history.back();"
+						+"</script>";
+			writer.println(script);
+		}
+		catch (Exception e) {
+		}
+	}
+
 	public static void alertLocation(String msg, String url, JspWriter out) {
 		try {
 			String script = ""
@@ -28,4 +61,5 @@ public class JSFunction {
 		catch (Exception e) {
 		}
 	}
+
 }
